f4728b116027b25b2b0aa5dbbda9ac8a129a937d
allenLQVE <allenlqve@gmail.com>
2024-07-24 16:27:09 -0400

    serializers initial

---
+++ receipts/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from receipts.models import Item, Store, PurchaseRecord
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = '__all__'
+
+class StoreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Store
+        fields = '__all__'
+
+class PurchaseRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        mdoel = PurchaseRecord
+        fields = '__all__'
