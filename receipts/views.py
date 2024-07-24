53d04a2e54813bb0f6c6d88efca7705adbd326d5
allenLQVE <allenlqve@gmail.com>
2024-07-24 16:37:55 -0400

    views initial

--- receipts/views.py
+++ receipts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+
+from ReceiptRecorder.receipts.models import Item, Store, PurchaseRecord
+from ReceiptRecorder.receipts.serializers import ItemSerializer, StoreSerializer, PurchaseRecordSerializer
+
 # Create your views here.
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+class StoreViewSet(viewsets.ModelViewSet):
+    queryset = Store.objects.all()
+    serializer_class = StoreSerializer
+
+class PurchaseRecordViewSet(viewsets.ModelViewSet):
+    queryset = Store.objects.all()
+    serializer_class = PurchaseRecordSerializer
