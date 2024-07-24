3ac5a0c48e55c0710fd8140a1cc2b2622cd1c2fd
allenLQVE <allenlqve@gmail.com>
2024-07-24 16:48:19 -0400

    routers

--- receiptRecorder/urls.py
+++ receiptRecorder/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from rest_framework import routers
+
+from ReceiptRecorder.receipts.views import ItemViewSet, StoreViewSet, PurchaseRecordViewSet
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'items', ItemViewSet)
+router.register(r'stores', StoreViewSet)
+router.register(r'records', PurchaseRecordViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
